9660eb63aba3ed7c6256d2b875808830e3a21ea5
Dmytro Krasnikov <dmytrokrasnikov3@gmail.com>
2026-03-18 22:57:34 +0100

    додано нові запити

--- Academy/CREATE DATABASE Academy_New;.sql
+++ Academy/CREATE DATABASE Academy_New;.sql
@@ -84,3 +84,73 @@ ALTER TABLE Groups
 ADD CONSTRAINT CHK_Groups_Year_New CHECK (Year BETWEEN 1 AND 5);
 
 
+SELECT COUNT(DISTINCT T.Id) AS TeachersCount
+FROM Teachers T
+JOIN Lectures L ON T.Id = L.TeacherId
+JOIN GroupsLectures GL ON L.Id = GL.LectureId
+JOIN Groups G ON GL.GroupId = G.Id
+JOIN Departments D ON G.DepartmentId = D.Id
+WHERE D.Name = N'Software Development';
+
+SELECT COUNT(L.Id) AS LecturesCount
+FROM Lectures L
+JOIN Teachers T ON L.TeacherId = T.Id
+WHERE T.Name = N'Dave' AND T.Surname = N'McQueen';
+
+SELECT COUNT(Id) AS LecturesCount
+FROM Lectures
+WHERE LectureRoom = N'D201';
+
+SELECT LectureRoom, COUNT(Id) AS LecturesCount
+FROM Lectures
+GROUP BY LectureRoom;
+
+SELECT COUNT(DISTINCT GL.GroupId) AS GroupsCount
+FROM GroupsLectures GL
+JOIN Lectures L ON GL.LectureId = L.Id
+JOIN Teachers T ON L.TeacherId = T.Id
+WHERE T.Name = N'Jack' AND T.Surname = N'Underhill';
+
+SELECT AVG(T.Salary) AS AverageSalary
+FROM Teachers T
+JOIN Lectures L ON T.Id = L.TeacherId
+JOIN GroupsLectures GL ON L.Id = GL.LectureId
+JOIN Groups G ON GL.GroupId = G.Id
+JOIN Departments D ON G.DepartmentId = D.Id
+JOIN Faculties F ON D.FacultyId = F.Id
+WHERE F.Name = N'Computer Science';
+
+SELECT MIN(StudentCount) AS MinStudents, MAX(StudentCount) AS MaxStudents
+FROM Groups;
+
+SELECT AVG(Financing) AS AverageFinancing
+FROM Departments;
+
+SELECT T.Name + ' ' + T.Surname AS FullName, COUNT(DISTINCT L.SubjectId) AS SubjectsCount
+FROM Teachers T
+JOIN Lectures L ON T.Id = L.TeacherId
+GROUP BY T.Id, T.Name, T.Surname;
+
+SELECT DayOfWeek, COUNT(Id) AS LecturesCount
+FROM Lectures
+GROUP BY DayOfWeek;
+
+SELECT L.LectureRoom, COUNT(DISTINCT D.Id) AS DepartmentsCount
+FROM Lectures L
+JOIN GroupsLectures GL ON L.Id = GL.LectureId
+JOIN Groups G ON GL.GroupId = G.Id
+JOIN Departments D ON G.DepartmentId = D.Id
+GROUP BY L.LectureRoom;
+
+SELECT F.Name, COUNT(DISTINCT L.SubjectId) AS SubjectsCount
+FROM Faculties F
+JOIN Departments D ON F.Id = D.FacultyId
+JOIN Groups G ON D.Id = G.DepartmentId
+JOIN GroupsLectures GL ON G.Id = GL.GroupId
+JOIN Lectures L ON GL.LectureId = L.Id
+GROUP BY F.Name;
+
+SELECT T.Name + ' ' + T.Surname AS TeacherName, L.LectureRoom, COUNT(L.Id) AS LecturesCount
+FROM Teachers T
+JOIN Lectures L ON T.Id = L.TeacherId
+GROUP BY T.Id, T.Name, T.Surname, L.LectureRoom;
